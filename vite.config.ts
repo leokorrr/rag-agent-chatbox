239652500354568a48cc002cd4b8809094b9a469
Leo <leokorrr@gmail.com>
2025-02-28 15:21:13 +0100

    feat: build vercel 3

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import tailwindcss from 'tailwindcss'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  build: {
-    outDir: 'dist',
-    rollupOptions: {
-      input: 'src/index.html'
+  plugins: [react()],
+  css: {
+    postcss: {
+      plugins: [tailwindcss]
     }
+  },
+  build: {
+    outDir: 'dist'
   }
 })
